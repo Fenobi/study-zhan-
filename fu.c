890c64dc2fedf78b94af8c82c5dd5d0f54d22b30
Fenobi <13667739122@qq.com>
2022-06-23 00:20:23 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "stack.h"
+
+void test1()
+{
+	ST st;
+	StackInit(&st);
+
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+	StackPush(&st, 4);
+	StackPush(&st, 4);
+	StackPush(&st, 4);
+	StackPush(&st, 4);
+	StackPush(&st, 4);
+	StackPush(&st, 4);
+
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+	
+	
+
+
+	StackDestroy(&st);
+}
+
+
+int main()
+{
+	test1();
+	return 0;
+}
