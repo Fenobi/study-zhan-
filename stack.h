701c9e35e399db1fc8cc903033e8156573252e04
Fenobi <13667739122@qq.com>
2022-08-07 11:07:36 +0800

    改错

--- stack.h
+++ stack.h
@@ -13,13 +13,27 @@ typedef struct Stack
 	STDateType* a;
 	//栈顶
 	int top;
+	//容量
 	int capacity;
 }ST;
 
+//初始化栈
 void StackInit(ST* ps);
+
+//销毁栈
 void StackDestroy(ST* ps);
-void StackPush(ST* ps,STDateType x);
+
+//入栈
+void StackPush(ST* ps, STDateType x);
+
+//出栈
 void StackPop(ST* ps);
+
+//获取栈顶元素
 STDateType StackTop(ST* ps);
+
+//获取栈内元素个数
 int StackSize(ST* ps);
+
+//检测栈是否为空，空则返回非零结果，如果不空则返回0
 bool StackEmpty(ST* ps);
